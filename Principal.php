81cefeb5f681e6b261784706d8e82fd4968bbd6b
Yuri Domingos <64822018+YuriDomingos@users.noreply.github.com>
2021-08-25 09:40:53 +0100

    Update Principal.php

--- Principal.php
+++ Principal.php
@@ -3,3 +3,31 @@
 Data : 25- 08 -2021
 Objectivo : Resolver bags do wordpress
 /
+//** *Enable upload for webp image files.*/
+function webp_upload_mimes($existing_mimes) {
+    $existing_mimes['webp'] = 'image/webp';
+    return $existing_mimes;
+}
+add_filter('mime_types', 'webp_upload_mimes');
+
+
+//** * Enable preview / thumbnail for webp image files.*/
+function webp_is_displayable($result, $path) {
+    if ($result === false) {
+        $displayable_image_types = array( IMAGETYPE_WEBP );
+        $info = @getimagesize( $path );
+
+        if (empty($info)) {
+            $result = false;
+        } elseif (!in_array($info[2], $displayable_image_types)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+    }
+
+    return $result;
+}
+add_filter('file_is_displayable_image', 'webp_is_displayable', 10, 2);
+
+
